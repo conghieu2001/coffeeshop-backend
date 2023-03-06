f3a1ab732e4444122fbb3a2974f657404f4a0ddb
conghieu2001 <conghieu116st@gmail.com>
2023-03-06 16:01:28 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const itemsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/items", itemsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to coffee shop applicaton"});
